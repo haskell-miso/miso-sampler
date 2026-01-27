81461d4e6bd077f2423670cdf409143ce653ca47
Andreas Ländle <alaendle@users.noreply.github.com>
2026-01-27 17:46:17 +0100

    Added HLS devShell (#9)

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
   outputs = inputs:
     inputs.miso.inputs.flake-utils.lib.eachDefaultSystem (system: {
       devShell = inputs.miso.outputs.devShells.${system}.default;
+      devShells.hls = inputs.miso.outputs.devShells.${system}.hls;
       devShells.wasm = inputs.miso.outputs.devShells.${system}.wasm;
       devShells.ghcjs = inputs.miso.outputs.devShells.${system}.ghcjs;
     });
